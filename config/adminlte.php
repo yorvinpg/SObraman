cb08e35c0d6b1985af9d7161424d3ebba23c5814
yorvinpg <yorvin_20_1997@hotmail.com>
2023-06-06 10:31:44 -0500

    update

--- config/adminlte.php
+++ config/adminlte.php
@@ -192,7 +192,7 @@ return [
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'sidebar-dark-info elevation-4',
+    'classes_sidebar' => 'sidebar-dark-orange elevation-4',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
