e35943a68a0c5e8d800fc96f0635b188837a1217
yorvinpg <yorvin_20_1997@hotmail.com>
2023-06-18 00:29:03 -0500

    act

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -41,7 +41,7 @@ class DashController extends Controller
         $countC = Solicitudot::where('idEstado', '5')
             ->count();
         if ($total > 0) {
-            $porcentajeC =   ceil(($countC / $total) * 100);
+            $porcentajeC =   floor(($countC / $total) * 100);
         } else {
             $porcentajeC = 0;
         }
